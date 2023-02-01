41f9544a79951f497f50772112dad922f46d036a
DINESH KUMAR MAJHI <30789050+D1N3SH-DEV@users.noreply.github.com>
2023-02-01 19:44:55 +0530

    Hands-on assignment
    Assignment from Coursera

---
+++ Graded Assignment Project.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # **My Jupyter Notebook on IBM Watson Studio**
+
+# ## **Dinesh Kumar Majhi**
+
+# ### *I am interested in Data Science because I like to dive deep into data and make some remarkable symantics of it.*
+
+# ### Program below shows the largest number among the set:
+
+# In[4]:
+
+
+n1 = 13
+n2 = 21
+n3 = 17
+if(n1>=n2) and (n1>=n3):
+    l=n1
+elif(n2>=n1) and (n2>=n3):
+    l=n2
+else:
+    l=n3
+print("Largest -->", l)
+
+
+# ***
+# This is horizontal rule.
+# ***
+
+# ### **Bullet List**:
+# 
+# * This
+# * is
+# * a
+# * bullet
+# * list
+
+# ## **Numbered List:**
+# 1. This
+# 2. is
+# 3. a
+# 4. numbered
+# 5. list
+
+# ## **Table:**
+
+# |Brand| |Category| |Origin|
+# |-----| |--------| |------|
+# |Puma| |Sports| |Germany|
+# |Sony| |Electronics & Media| |Japan|
+# |Cadbury| |Food| |United Kingdom|
+
+# ## **Hyperlink:**
+
+# Click here --> [Google](#www.google.com)
+
+# ## **Image:**
+
+# ![lance-wilkinson-finalcomp.jpg](attachment:lance-wilkinson-finalcomp.jpg)
+
+# ## **Blocked Quotes:**
+
+# > I am trying to type a block quote.
+# >
+# >>This is sub-section. 
+
+# ## **Strikethrough:**
+
+# ~~Error 404~~
+
+# In[ ]:
+
+
+
+
